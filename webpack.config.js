b3e5de05c052f173bc20b125e5e5aabb89ac42e4
Alexander <alexandertisko@hotmail.com>
2020-10-25 22:25:02 -0300

    prueba

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+    entry: './App/assets/scripts/App.js'
+}
+const path = require('path')
+module.exports = {
+    entry: './App/assets/scripts/App.js', 
+    mode:'development',
+    module: {
+        rules:[
+            {
+                test:/\.css$/i,
+                use: ['style-loader','css-loader', {loader:'postcss-loader', options: {plugins: postCSSPlugins}}]
+            }
+        ]
+    }
+    output: {
+        filename: 'bundled.js', 
+        path: path.resolve(__dirname, 'app')
+    },
+    mode: 'development',
+	watch:true
+}
